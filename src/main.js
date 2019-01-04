8d13b69404c949ff1915addfb81ac4a654268d9d
Sol <faisol@tellscore.com>
2019-01-04 18:48:10 +0700

    Update

--- src/main.js
+++ src/main.js
@@ -2,9 +2,37 @@ const server = require('express')
 const request = require('request')
 const bodyParser = require('body-parser')
 
+const PORT = process.env.PORT || 3000
+
 const router = server.Router()
 
-const PORT = process.env.PORT || 3000
+router.post('/', (req, res, next) => {
+  const token = req.body.token
+  const msg = req.body.msq
+
+  request({
+    method: 'POST',
+    uri: 'https://notify-api.line.me/api/notify',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    auth: {
+      'bearer': token
+    },
+    form: {
+      msg
+    }
+  }, (err, httpResponse, body) => {
+    if(err) {
+      console.error(err)
+    } else {
+      res.json({
+        httpResponse,
+        body
+      })
+    }
+  })
+})
 
 server()
   .use(bodyParser.json())
